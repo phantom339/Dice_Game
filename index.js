b4699f8919c48c0236318cdfb115d33e3dcff287
phantom339 <pulkitkathayat339@gmail.com>
2023-10-23 13:47:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1= Math.floor((Math.random() * 6) + 1);
+var randomNumber2= Math.floor((Math.random() * 6) + 1);
+var randomImage2="images/dice"+randomNumber2+".png";
+var randomDiceName="dice"+randomNumber1+".png";
+var randomImage="images/"+randomDiceName;
+document.querySelectorAll("img")[0].setAttribute("src",randomImage);
+document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="🚩 Player1 wins";
+}
+else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML="🚩 Player2 wins";
+}
+else{
+    document.querySelector("h1").innerHTML="😎draw";
+}
